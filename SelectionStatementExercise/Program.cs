61e71cbe6c20b4572d8f35ca025d0c8de7a1c1bd
Eric Hartley <spiffydude0@gmail.com>
2024-08-06 14:58:01 -0500

    completed exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.Write("What is your favorite number?: ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Really, human? This is your favorite number?");
+            }
+            else 
+            { 
+                Console.WriteLine("Hmmm... I thought it was your favorite number...");
+            }
+
         }
     }
 }
